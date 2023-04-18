822ce4ab3e44abaec715bfdb9c35619aab1196a1
Noah19-cell <103570425+Noah19-cell@users.noreply.github.com>
2023-04-18 21:19:57 +0200

    final changes to Challege 1

--- Challenge 1/filse.js
+++ Challenge 1/filse.js
@@ -1,15 +1,17 @@
 firstName = 'John';
 age = 35;
-hobby = 'Coding';
+const hobby = 'Coding';
 
 const logTwice = () => {
   console(parameter)
   console(parameter)
 }
 
-function hobby () {
-  logTwice(`Hello, ${name} (${age}). I love ${hobby}!`)
+function logHobby () {
+  logTwice(`Hello, ${firstName} (${age}). I love ${hobby}!`)
+ 
 }
 
-hobby()
+ logHobby()
+
  
